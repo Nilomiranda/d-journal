d52b9fa40d9ced02aa67a951ebf75d30e6a7e248
Nilomiranda <nilomiranda3@gmail.com>
2023-04-17 21:51:05 -0300

    chore: setup github client for request to the API

---
+++ src/resources/github.ts
@@ -0,0 +1,73 @@
+import type { AxiosResponse } from 'axios';
+import axios from 'axios';
+
+const githubHttpClient = axios.create({
+    baseURL: 'https://api.github.com/repos/nilomiranda/d-journal/issues',
+    headers: {
+        Accept: '',
+        'X-GitHub-Api-Version': '2022-11-28',
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    },
+})
+
+interface ListGithubIssuesParams {
+    page?: number;
+}
+
+interface GithubUser {
+    login: string;
+    id: number;
+}
+
+interface GithubLabel {
+    id: number;
+    name: string;
+    
+    /**
+     * HEX CODE
+     */
+    color: string;
+}
+
+interface GithubIssue {
+    url: string;
+    id: number;
+    number: number;
+    user: GithubUser;
+    labels: GithubLabel[];
+    title: string;
+    body: string;
+}
+
+const GithubIssues = {
+    async list({ page = 1 }: ListGithubIssuesParams): Promise<GithubIssue[]> {
+        try {
+            const response = await githubHttpClient.get<any, AxiosResponse<GithubIssue[]>>('', {
+                params: {
+                    state: 'open',
+                    creator: 'nilomiranda',
+                    labels: 'blog-post',
+                    direction: 'desc',
+                    per_page: 10,
+                    page,
+                }
+            });
+
+            return response.data ?? [];
+        } catch (err) {
+            console.error('Error listing posts.', err);
+            throw err;
+        }
+    },
+
+    async read(): Promise<GithubIssue> {
+        try {
+            const response = await githubHttpClient.get<any, AxiosResponse<GithubIssue>>('');
+
+            return response.data;
+        } catch (err) {
+            console.error('Error reading post.', err);
+            throw err;
+        }
+    },
+}
