a84c0471d0898d7f1c953e8e159b6c0be865b3df
Nilomiranda <nilomiranda3@gmail.com>
2023-04-17 22:37:00 -0300

    chore: install and setup vanilla-css-extract for styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), vanillaExtractPlugin()]
 });
