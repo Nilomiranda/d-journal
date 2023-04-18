b90c7977d9f669e5dcb383f70ec1ac4d136fccb0
Nilomiranda <nilomiranda3@gmail.com>
2023-04-17 22:37:23 -0300

    chore: initial styling of post list element

---
+++ src/components/PostListElement.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+export const Card = style({
+    display: 'flex',
+    flexDirection: 'column'
+})
---
+++ src/components/PostListElement.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {format} from "date-fns"
+	import type { Post } from "../resources/github";
+    import { Card } from "./PostListElement.css";
+
+    export let post: Post;
+</script>
+
+<div class={Card}>
+    <small>{format(new Date(post.created_at), 'LLLL do, yyyy')}</small>
+    <strong>{post.title}</strong>
+</div>
