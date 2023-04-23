e1728a55d0b38976488c6eac5c593b34638a78c9
Nilomiranda <nilomiranda3@gmail.com>
2023-04-23 11:46:17 -0300

    chore: more global styling

--- src/routes/layout.css.ts
+++ src/routes/layout.css.ts
@@ -3,3 +3,7 @@ import { globalStyle } from "@vanilla-extract/css";
 globalStyle('h1', {
     fontFamily: 'Roboto Slab, serif',
 })
+
+globalStyle('p, small, strong', {
+    fontFamily: 'Arial, sans-serif',
+})
