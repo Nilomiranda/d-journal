36d7b2012db0e0d10310ceb6ae6a7476e02911db
Nilomiranda <nilomiranda3@gmail.com>
2023-04-23 18:15:46 -0300

    chore: main metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script>
+	import { page } from "$app/stores";
 	import PostListElement from "../components/PostListElement.svelte";
 	import { Main } from "./page.css";
 
     export let data;
 </script>
+
+<svelte:head>
+    <title>Danilo Journal</title>
+
+    <meta property="og:title" content="Danilo Journal">
+    <meta property="og:image" content={`${$page.url.origin}/favicon.png`}>
+    <meta property="og:description" content="Danilo is sharing his thoughts">
+    <meta property="og:type" content="website">
+</svelte:head>
 
 <main class={Main}>
     <h1>Danilo Journal</h1>
